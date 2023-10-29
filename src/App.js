c8a716815458bd7e98d0f47d2d6c14b551e99a00
Rathish <rathishsenthilkumar4@gmail.com>
2023-10-29 15:29:30 +0530

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Header from "./header"
+import Content from'./Content'
+import Footer from './footer'
 
 function App() {
+  const handleNameChange=()=>{
+    const names=["rathish","rakshitha","John"];
+    const int=Math.floor(Math.random()*3);
+    return names[int];
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header/>
+      <Content/>
+      <Footer/>
+
+
+     
     </div>
   );
 }
